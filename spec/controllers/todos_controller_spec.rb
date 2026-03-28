e9e469daa7fa4234e4be5c624fbec1160d96a153
Alef ojeda de Oliveira <alef.oliveira@siedos.com.br>
2026-03-28 15:12:58 -0300

    refactor(dummy): use button_to for highlight action

--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -19,4 +19,25 @@ RSpec.describe TodosController, type: :controller do
       post :highlight, params: { id: todo.id }, format: :js
     end
   end
+
+  describe 'GET #datatable' do
+    render_views
+
+    it 'renders destacar action as a post form button' do
+      user = create(:user)
+      todo = create(:todo, user_id: user.id)
+
+      allow(controller).to receive(:authenticate_user!).and_return(true)
+      allow(controller).to receive(:current_user).and_return(user)
+
+      get :datatable
+
+      html = Nokogiri::HTML(response.body)
+      form = html.at_css("form[action='#{highlight_todo_path(todo, format: :js)}']")
+
+      expect(form).not_to be_nil
+      expect(form['method']).to eq('post')
+      expect(form.at_css("button[type='submit']")&.text.to_s).to include('Destacar')
+    end
+  end
 end
