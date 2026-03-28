3c9a8e24f9e61f62f6842768aa8a86c2a419c477
Alef ojeda de Oliveira <alef.oliveira@siedos.com.br>
2026-03-28 10:17:23 -0300

    test: broaden broadcaster dispatch coverage

--- spec/models/concerns/broadcast_hub/broadcaster_spec.rb
+++ spec/models/concerns/broadcast_hub/broadcaster_spec.rb
@@ -11,20 +11,24 @@ RSpec.describe BroadcastHub::Broadcaster, type: :model do
     BroadcastHub.configuration = original_configuration
   end
 
-  describe "#broadcast_dispatch" do
-    it "broadcasts a dispatch payload with event details" do
+  describe "broadcast helpers" do
+    let(:todo) { create(:todo, :with_user) }
+
+    before do
       BroadcastHub.configure do |config|
         config.allowed_resources = [ "todo" ]
         config.authorize_scope = ->(_context) { true }
         config.stream_key_resolver = ->(_context) { "resource:todo:user:1" }
       end
 
-      todo = create(:todo, :with_user)
-
       allow(ActionCable.server).to receive(:broadcast)
+    end
 
+    it "broadcast_dispatch skips content rendering and includes event details" do
       event_name = "todo:highlight"
       event_data = { todo_id: todo.id, urgent: true }
+
+      expect(todo).not_to receive(:render_broadcast_content)
 
       todo.broadcast_dispatch("#todos", event_name, event_data)
 
@@ -36,6 +40,39 @@ RSpec.describe BroadcastHub::Broadcaster, type: :model do
           content: nil,
           event_name: event_name,
           event_data: event_data,
+          id: "todo_#{todo.id}"
+        )
+      )
+    end
+
+    it "broadcast_append renders content and forwards append action" do
+      allow(todo).to receive(:render_broadcast_content).and_return("<li>Updated Todo</li>")
+
+      todo.broadcast_append("#todos")
+
+      expect(todo).to have_received(:render_broadcast_content).once
+      expect(ActionCable.server).to have_received(:broadcast).with(
+        "resource:todo:user:1",
+        hash_including(
+          action: "append",
+          target: "#todos",
+          content: "<li>Updated Todo</li>",
+          id: "todo_#{todo.id}"
+        )
+      )
+    end
+
+    it "broadcast_remove keeps nil content" do
+      expect(todo).not_to receive(:render_broadcast_content)
+
+      todo.broadcast_remove("#todo_#{todo.id}")
+
+      expect(ActionCable.server).to have_received(:broadcast).with(
+        "resource:todo:user:1",
+        hash_including(
+          action: "remove",
+          target: "#todo_#{todo.id}",
+          content: nil,
           id: "todo_#{todo.id}"
         )
       )
